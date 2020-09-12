72ce988065cd5b731a2be3b69063b1fb03c2ffe0
ArsalaBangash <arsalabangash@gmail.com>
2020-09-12 03:23:11 -0400

    Stated repos explicitly

--- labelsync.ts
+++ labelsync.ts
@@ -9,7 +9,15 @@ labelsync({
   repos: {
     /* Check presets in the repos folder. */
     // prisma,
-    "*": github,
+    "toonin": github,
+    "org": github,
+    "material-math": github,
+    "grey.software": github,
+    "futurist.network": github,
+    "sponsorware": github,
+    "twitter-focus": github,
+    "linkedin-focus": github,
+    /* Personalized repositories */
     /* Personalized repositories */
   },
 });
